e4d0f9ed832488e97399ad4a08e6dd9d0e17e432
Kasey Nguyen <kaseynguyen0527@gmail.com>
2023-06-28 16:21:16 -0700

    Updated webpack config/ installed dependencies/ updated panel.ts and webview.ts
    Co-authored-by: Christopher Park <cp3063@columbia.edu>
    Co-authored-by: Ulf Wong <uwong8@gmail.com>
    Co-authored-by: Abe <https://github.com/a-hend>
    Co-authored-by: Yosuke Tomita <ytomita615@gmail.com>

---
+++ src/webview/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Whatsup team sGlaceon!!!!!</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+#test {
+  background-color: rgb(32, 23, 148);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
