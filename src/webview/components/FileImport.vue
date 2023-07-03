c72f7eb740d63632185797853adf0908c6e84fe0
Chris Park <cp3063@columbia.edu>
2023-07-03 10:39:51 -0700

    Input for entry file

---
+++ src/webview/components/FileImport.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <input type="file" @change="handleFileUpload">
+      <button @click="uploadFile">Upload</button>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+        data() {
+            return {
+                selectedFile: null
+            };
+        },
+        methods: {
+            handleFileUpload(event) {
+                this.selectedFile = event.target.files[0];
+                console.log(event.target)
+            },
+            uploadFile() {
+                if (this.selectedFile) {
+                // Perform file upload logic here
+                console.log("Uploading file:", this.selectedFile);
+                } else {
+                console.log("No file selected");
+                }
+            }
+        }
+    }
+  </script>
+  
